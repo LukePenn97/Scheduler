86657e3ddea6b353229e6a81d5f059318f89d508
Luke Penner <lukepenner@hotmail.ca>
2021-05-19 21:40:44 -0700

    DayListItem finished with formatSpots function

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames"
+
+export default function DayListItem(props) {
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return 'no spots remaining';
+    }
+    if (spots === 1) {
+      return '1 spot remaining';
+    }
+    if (spots > 1) {
+      return `${spots} spots remaining`;
+    }
+  }
+  return (
+    <li className={dayClass} data-testid="day" onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light" id={props.name}>{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
